4c9277312524eaf67afd75b774cd31aa4c8e13b0
R Gokul Krishna <98410585+gokul1974@users.noreply.github.com>
2022-02-26 15:18:39 +0530

    Add files via upload

---
+++ Experiment 6/FileNotFound.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+public class FileNotFound {
+    
+    
+        public static void main(String args[]) throws IOException {
+        try{
+            FileReader fin = new FileReader("input.txt");
+        }catch(FileNotFoundException e){System.out.println("File not Found in directory");} 
+        }
+        
+    }
+    
+
